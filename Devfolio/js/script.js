97be89b793f3e8cf98ec93b91ba8563d451e51e8
Mann Patel <130435633+Mann-Patel-1@users.noreply.github.com>
2024-05-07 17:51:48 -0600

    added the tabs and made the theme button work

--- Devfolio/js/script.js
+++ Devfolio/js/script.js
@@ -4,3 +4,24 @@
  */
 
 "use strict";
+
+//dark and light mode
+const /**{NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
+const /**{NodeElement} */ $HTML = document.documentElement;
+let /**{Boolean | String} */ isdark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")) {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isdark ? "dark" : "light";
+//   sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+const changeTheme = () => {
+    $HTML.dataset.theme = sessionStorage.getItem("theme") == "light" ? "dark": "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+
+}
+
+$themeBtn.addEventListener("click", changeTheme);
+
