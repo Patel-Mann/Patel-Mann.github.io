5d72b5afacac562010e05ef018181878ce9130e3
Mann Patel <mann.patel1@ucalgary.ca>
2024-06-15 21:31:42 -0700

    mostly done with the website tutorial

--- js/script.js
+++ js/script.js
@@ -22,6 +22,28 @@ const changeTheme = () => {
     sessionStorage.setItem("theme", $HTML.dataset.theme);
 
 }
-
 $themeBtn.addEventListener("click", changeTheme);
 
+
+
+
+
+const $tabBtn = document.querySelectorAll("[data-tab-btn]");
+
+let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item =>{
+  item.addEventListener("click", function (){
+    lastActiveTab.classList.remove("active");
+    lastActiveTabBtn.classList.remove("active");
+
+    const $tabContent = document.querySelector(`[data-tab-content = "${item.dataset.tabBtn}"]`);
+    $tabContent.classList.add("active");
+    this.classList.add("active");
+
+    lastActiveTab = $tabContent;
+    lastActiveTabBtn = this;
+
+  });
+});
