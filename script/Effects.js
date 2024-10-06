af1172420b98f0bffe0139c3a0de299d1028bb69
Mann Patel <130435633+MannPatel0@users.noreply.github.com>
2024-10-06 00:46:26 -0600

    404 screen added, updated blog, resume and email

--- script/Effects.js
+++ script/Effects.js
@@ -7,15 +7,15 @@ function randomInteger(min, max) {
 function rgb(r, g, b) {
     return new THREE.Vector3(r, g, b);
 }
+
 document.addEventListener("DOMContentLoaded", function(e) {
-   
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement )
-    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
     let vCheck = false;
 
     camera.position.z = 5;
@@ -23,25 +23,26 @@ document.addEventListener("DOMContentLoaded", function(e) {
     var randomisePosition = new THREE.Vector2(1, 2);
 
     var R = function(x, y, t) {
-        return( Math.floor(192 + 64*Math.cos( (x*x-y*y)/300 + t )) );
-    }
-     
+        return Math.floor(192 + 64 * Math.cos((x * x - y * y) / 300 + t));
+    };
+
     var G = function(x, y, t) {
-        return( Math.floor(192 + 64*Math.sin( (x*x*Math.cos(t/4)+y*y*Math.sin(t/3))/300 ) ) );
-    }
-      
+        return Math.floor(192 + 64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300));
+    };
+
     var B = function(x, y, t) {
-        return( Math.floor(192 + 64*Math.sin( 5*Math.sin(t/9) + ((x-100)*(x-100)+(y-100)*(y-100))/1100) ));
-    }
-    let sNoise = document.querySelector('#snoise-function').textContent
+        return Math.floor(192 + 64 * Math.sin(5 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100));
+    };
+
+    let sNoise = document.querySelector('#snoise-function').textContent;
     let geometry = new THREE.PlaneGeometry(window.innerWidth / 2, 400, 100, 100);
     let material = new THREE.ShaderMaterial({
         uniforms: {
-            u_bg: {type: 'v3', value: rgb(162, 138, 241)},
-            u_bgMain: {type: 'v3', value: rgb(162, 138, 241)},
-            u_color1: {type: 'v3', value: rgb(162, 138, 241)},
-            u_color2: {type: 'v3', value: rgb(82, 31, 241)},
-            u_time: {type: 'f', value: 0},
+            u_bg: { type: 'v3', value: rgb(162, 138, 241) },
+            u_bgMain: { type: 'v3', value: rgb(162, 138, 241) },
+            u_color1: { type: 'v3', value: rgb(162, 138, 241) },
+            u_color2: { type: 'v3', value: rgb(82, 31, 241) },
+            u_time: { type: 'f', value: 0 },
             u_randomisePosition: { type: 'v2', value: randomisePosition }
         },
         fragmentShader: sNoise + document.querySelector('#fragment-shader').textContent,
@@ -56,31 +57,32 @@ document.addEventListener("DOMContentLoaded", function(e) {
     mesh.rotationZ = 0.1;
     scene.add(mesh);
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
+
     let t = 0;
     let j = 0;
     let x = randomInteger(0, 32);
     let y = randomInteger(0, 32);
+
     const animate = function () {
-        requestAnimationFrame( animate );
-        renderer.render( scene, camera );
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
         mesh.material.uniforms.u_randomisePosition.value = new THREE.Vector2(j, j);
-        
-        mesh.material.uniforms.u_color1.value = new THREE.Vector3(R(x,y,t/2), G(x,y,t/2), B(x,y,t/2));
+
+        mesh.material.uniforms.u_color1.value = new THREE.Vector3(R(x, y, t / 2), G(x, y, t / 2), B(x, y, t / 2));
 
         mesh.material.uniforms.u_time.value = t;
-        if(t % 0.1 == 0) {         
-            if(vCheck == false) {
+        if (t % 0.1 == 0) {
+            if (vCheck == false) {
                 x -= 1;
-                if(x <= 0) {
+                if (x <= 0) {
                     vCheck = true;
                 }
             } else {
                 x += 1;
-                if(x >= 32) {
+                if (x >= 32) {
                     vCheck = false;
                 }
-
             }
         }
 
@@ -89,5 +91,11 @@ document.addEventListener("DOMContentLoaded", function(e) {
         t = t + 0.05;
     };
     animate();
-  
+
+    window.addEventListener('resize', function () {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.render(scene, camera);
+    });
 });
